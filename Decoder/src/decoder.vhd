af111e1ef8a846703e80365b08511d02091d35d5
amarkh <62019370+amarkh@users.noreply.github.com>
2023-12-17 09:29:16 -0500

    Initial error free code before simulation

--- Decoder/src/decoder.vhd
+++ Decoder/src/decoder.vhd
@@ -1,11 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity decoder is
     port (
         clk, rst        : in  std_logic;
         inputA, inputB  : in  std_logic;
-        en, done        : out std_logic
+        output        : out std_logic_vector(15 downto 0)
         );
 end decoder;
 
@@ -14,67 +15,33 @@ end decoder;
 -- notes.pdf
 
 architecture two_process of decoder is
-
-    type state_t is (START, COMPUTE, FINISH, RESTART);
-    signal state_r, next_state : state_t;
-    
+    signal temp : integer range -32767 to 32768;
 begin
 
-    process(clk, rst)
+    -- This process is to reset the signal value any time reset is true
+    -- There shouldn't be a clock input for simply connecting an encoder to a FPGA
+    -- It doesn't hurt to leave it in there.
+    -- process(clk, rst)
+    -- begin
+    --     if (rst = '1') then
+    --         temp <= 0;
+    --     -- elsif (rising_edge(clk)) then
+
+    --     end if;
+    -- end process;
+
+    process (rst, inputA, inputB)
     begin
         if (rst = '1') then
-            state_r <= START;
-        elsif (rising_edge(clk)) then
-            state_r <= next_state;
+            temp <= 0;
+        elsif(inputA = '1' and inputB = '0') then
+            temp <= temp + 1;
+        elsif (inputA = '0' and inputB = '1') then
+            temp <= temp - 1;
         end if;
+
     end process;
 
-    process (state_r, go, ack)
-    begin
-        case (state_r) is
-            when START =>
-                if (go = '1') then
-                    done       <= '0';
-                    en         <= '0';
-                    next_state <= COMPUTE;
-                else
-                    done       <= '0';
-                    en         <= '0';
-                    next_state <= START;
-                end if;
+    output <= std_logic_vector(to_unsigned(temp, 16));
 
-            when COMPUTE =>
-                if (ack = '1') then
-                    en         <= '0';
-                    done       <= '1';
-                    next_state <= FINISH;
-                else
-                    en         <= '1';
-                    done       <= '0';
-                    next_state <= COMPUTE;
-                end if;
-
-            when FINISH =>
-                if (go = '1') then
-                    done       <= '1';
-                    en         <= '0';
-                    next_state <= FINISH;
-                else
-                    done       <= '1';
-                    en         <= '0';
-                    next_state <= RESTART;
-                end if;
-
-            when RESTART =>
-                if (go = '1') then
-                    done       <= '0';
-                    en         <= '0';
-                    next_state <= COMPUTE;
-                else
-                    done       <= '1';
-                    en         <= '0';
-                    next_state <= RESTART;
-                end if;
-        end case;
-    end process;
 end two_process;
